e977b90a8a886fb299e1cb524b7a97fedac5b768
muqhc <muqhc07@gmail.com>
2022-03-20 21:00:18 +0900

    some fix debugging

--- skygui-debug/src/main/kotlin/io/github/muqhc/skygui/debug/plugin/SkyguiDebugPlugin.kt
+++ skygui-debug/src/main/kotlin/io/github/muqhc/skygui/debug/plugin/SkyguiDebugPlugin.kt
@@ -4,7 +4,7 @@ import io.github.monun.kommand.kommand
 import io.github.muqhc.skygui.component.SkyComponent
 import io.github.muqhc.skygui.SkyDisplay
 import io.github.muqhc.skygui.debug.plugin.component.MeowPiano
-import io.github.muqhc.skygui.debug.plugin.manager.testDisplayManager
+import io.github.muqhc.skygui.manager.SimpleSkyguiManager
 import io.github.muqhc.skygui.util.Point
 import org.bukkit.Location
 import org.bukkit.event.Listener
@@ -13,7 +13,7 @@ import org.bukkit.scheduler.BukkitTask
 import org.bukkit.util.Vector
 
 class SkyguiDebugPlugin: JavaPlugin(), Listener {
-    val testDisplayManager = testDisplayManager()
+    val testDisplayManager = SimpleSkyguiManager()
     var playTask: BukkitTask? = null
     override fun onEnable() {
         kommand {
--- skygui-debug/src/main/kotlin/io/github/muqhc/skygui/debug/plugin/manager/TestDisplayManager.kt
+++
@@ -1,9 +0,0 @@
-package io.github.muqhc.skygui.debug.plugin.manager
-
-import io.github.muqhc.skygui.debug.plugin.SkyguiDebugPlugin
-import io.github.muqhc.skygui.manager.SimpleSkyguiManager
-
-fun SkyguiDebugPlugin.testDisplayManager(): SimpleSkyguiManager =
-    object: SimpleSkyguiManager() {
-
-    }
